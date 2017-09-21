02ae40ec59003f51bd1e0a8ed237e368aab6a41f
Suyeol Jeon <devxoul@gmail.com>
2017-09-21 21:05:47 +0900

    Migrate to Swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "CGFloatLiteral"
+  name: "CGFloatLiteral",
+  products: [
+    .library(name: "CGFloatLiteral", targets: ["CGFloatLiteral"]),
+  ],
+  targets: [
+    .target(name: "CGFloatLiteral"),
+    .testTarget(name: "CGFloatLiteralTests", dependencies: ["CGFloatLiteral"]),
+  ]
 )
